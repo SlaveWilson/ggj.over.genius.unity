8432ea17dda1a3f443e89bc03381e0541997da97
TNTommy <tommynhm@gmail.com>
2020-01-31 23:21:53 +0800

    Add Interactable base class

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public bool isInteractable = true;
+
+    public virtual void Interact()
+    {
+        Debug.Log("No Interact Action", gameObject);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,18 +4,38 @@ public class Player : MonoBehaviour
 {
     public float moveSpeed = 1.0f;
 
-    public Rigidbody2D rb;
+    private string TAG_INTERACTABLE = "Interactable";
 
-    Vector2 movement;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    private bool actionPressRelease;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     private void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        actionPressRelease = Input.GetKeyUp("space");
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up, 10.0f);
+        Debug.DrawRay(transform.position, Vector2.up);
+
+        if (hit.collider != null && hit.collider.gameObject.CompareTag(TAG_INTERACTABLE))
+        {
+            var interactObject = hit.collider.gameObject;
+            if (actionPressRelease)
+            {
+                interactObject.GetComponent<Interactable>().Interact();
+            }
+        }
     }
 }
