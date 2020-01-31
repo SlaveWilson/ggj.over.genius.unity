87416b46350d0397e1e7c15a61ac5ee3234ad920
TNTommy <tommynhm@gmail.com>
2020-02-01 02:06:23 +0800

    Make coop players

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,9 +4,19 @@ public class Player : MonoBehaviour
 {
     public float moveSpeed = 1.0f;
 
+    public string horizontalAxis = "Horizontal";
+    public string verticalAxis = "Vertical";
+
+    public KeyCode upKeyCode = KeyCode.W;
+    public KeyCode downKeyCode = KeyCode.S;
+    public KeyCode leftKeyCode = KeyCode.A;
+    public KeyCode rightKeyCode = KeyCode.D;
+
+    public KeyCode actionKeyCode = KeyCode.Space;
+
     private string TAG_INTERACTABLE = "Interactable";
 
-    private Interactable _activeItem = null;
+    private GameObject _activeItem = null;
 
     private Rigidbody2D _rb;
     private Vector2 _movement;
@@ -19,22 +29,22 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        _movement.x = Input.GetAxisRaw("Horizontal");
-        _movement.y = Input.GetAxisRaw("Vertical");
+        _movement.x = Input.GetAxisRaw(horizontalAxis);
+        _movement.y = Input.GetAxisRaw(verticalAxis);
 
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(upKeyCode))
             _facingDirection = Vector2.up;
-        else if (Input.GetKeyDown(KeyCode.S))
+        else if (Input.GetKeyDown(downKeyCode))
             _facingDirection = Vector2.down;
-        else if (Input.GetKeyDown(KeyCode.A))
+        else if (Input.GetKeyDown(leftKeyCode))
             _facingDirection = Vector2.left;
-        else if (Input.GetKeyDown(KeyCode.D))
+        else if (Input.GetKeyDown(rightKeyCode))
             _facingDirection = Vector2.right;
 
         RaycastHit2D hit = Physics2D.Raycast(transform.position, _facingDirection, 10.0f);
         Debug.DrawRay(transform.position, _facingDirection);
 
-        if (hit.collider != null && hit.collider.gameObject.CompareTag(TAG_INTERACTABLE) && Input.GetKeyUp("space"))
+        if (hit.collider != null && hit.collider.gameObject.CompareTag(TAG_INTERACTABLE) && Input.GetKeyUp(actionKeyCode))
         {
             var interactObject = hit.collider.gameObject;
             interactObject.GetComponent<Interactable>().Interact();
