fda01b4da96b18ec861883111a37a7863a42b6bd
TNTommy <tommynhm@gmail.com>
2020-02-01 00:25:03 +0800

    Impletement a basic timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,5 +2,35 @@
 
 public class Timer : MonoBehaviour
 {
-    
+    public float durationInSecond = 90.0f;
+    public bool isStop = true;
+
+    public float remainingTime;
+
+    private float _endTime = 0.0f;
+
+    private void FixedUpdate()
+    {
+        if (!isStop)
+        {
+            remainingTime = _endTime - Time.time;
+            if(remainingTime < 0)
+            {
+                isStop = true;
+            }
+        }
+    }
+
+    [ContextMenu("Start Timer")]
+    public void StartTimer()
+    {
+        isStop = false;
+        _endTime = Time.time + durationInSecond;
+    }
+
+    [ContextMenu("Stop Timer")]
+    public void StopTimer()
+    {
+
+    }
 }
