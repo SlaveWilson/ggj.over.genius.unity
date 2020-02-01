bc65a8f66376aa9235ba57c375fb795293abb79f
TNTommy <tommynhm@gmail.com>
2020-02-02 01:18:02 +0800

    Add trashcan animation

--- Assets/Scripts/Interactable/Trashcan.cs
+++ Assets/Scripts/Interactable/Trashcan.cs
@@ -1,10 +1,20 @@
-﻿public class Trashcan : Interactable
+﻿using UnityEngine;
+
+public class Trashcan : Interactable
 {
+    private Animator _anim;
+
+    private void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
     public override void Interact(Player player)
     {
         if (player.activeItem == null) return;
         if (player.activeItem.currentState != Item.State.Tool) return;
 
         player.activeItem = null;
+        _anim.SetTrigger("throwStuff");
     }
 }
