d9f1dedd933a46b543f673a7311a56e3fbafb9b4
TNTommy <tommynhm@gmail.com>
2020-02-02 00:36:41 +0800

    Update game scene

---
+++ Assets/Scripts/Interactable/Trashcan.cs
@@ -0,0 +1,10 @@
+﻿public class Trashcan : Interactable
+{
+    public override void Interact(Player player)
+    {
+        if (player.activeItem == null) return;
+        if (player.activeItem.currentState != Item.State.Tool) return;
+
+        player.activeItem = null;
+    }
+}
