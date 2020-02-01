f0390a4c450e14dce6cc4d699113d03e3a374a21
TNTommy <tommynhm@gmail.com>
2020-02-01 11:25:26 +0800

    Clean Folder Structure

---
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void StartZoomIn()
+    {
+        Debug.Log("StartZoomIn()");
+    }
+    public static void StartZoomOut()
+    {
+        Debug.Log("StartZoomOut()");
+    }
+}
