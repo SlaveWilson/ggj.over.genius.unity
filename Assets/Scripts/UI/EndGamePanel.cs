21168c3fe7103b6ecbe6515abc1c35b43715be88
TNTommy <tommynhm@gmail.com>
2020-02-02 12:29:41 +0800

    Tidy up folder structure

---
+++ Assets/Scripts/UI/EndGamePanel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class EndGamePanel : MonoBehaviour
+{
+    public GameObject ordersDelivered;
+    public GameObject ordersFailed;
+    public GameObject totalScore;
+    public GameObject playAgain;
+    public GameObject comingSoon;
+
+    // boolean
+    public static bool canPlayAgain = false;
+
+    private void Awake()
+    {
+        ordersDelivered = this.transform.Find("OrdersDelivered").gameObject;
+        ordersFailed = this.transform.Find("OrdersFailed").gameObject;
+        totalScore = this.transform.Find("TotalScore").gameObject;
+        playAgain = this.transform.Find("PlayAgain").gameObject;
+        comingSoon = this.transform.Find("ComingSoon").gameObject;
+    }
+
+    void Start()
+    {
+        ordersDelivered.GetComponent<TextMeshProUGUI>().text = Score.ordersDelivered.ToString() + " x 20 = " 
+        + (Score.ordersDelivered * 20).ToString();
+        ordersFailed.GetComponent<TextMeshProUGUI>().text = Score.ordersFailed.ToString() + " x 10 = "
+        + (Score.ordersFailed * 10).ToString();
+        totalScore.GetComponent<TextMeshProUGUI>().text = Score.totalScore.ToString();
+
+        playAgain.GetComponent<Button>().onClick.AddListener(
+        () => canPlayAgain = true);
+
+        comingSoon.GetComponent<Button>().onClick.AddListener(
+        () => { ComingSoon(); SoundManager.ClickButtonSE(); }); //canExit = true;
+    }
+
+    public void ComingSoon()
+    {
+        Destroy(gameObject);
+        Instantiate(CanvasManager.comingSoonPanelReference, DialogManager.canvasReference.transform);
+    }
+}
---
+++ Assets/Scripts/UI/Score.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    public static int ordersDelivered = 0;
+    public static int ordersFailed = 0;
+    public static int totalScore = 0;
+
+
+    // inspector
+    public GameObject _scorePanel;
+    public static GameObject scorePanelReference;
+
+    private void Awake()
+    {
+        scorePanelReference = _scorePanel;
+        ordersDelivered = 0;
+        ordersFailed = 0;
+        totalScore = 0;
+    }
+
+    void Update()
+    {
+        scorePanelReference.transform.Find("Score").GetComponent<TextMeshProUGUI>().text = totalScore.ToString();
+    }
+}
